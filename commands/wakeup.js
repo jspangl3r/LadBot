67d1473d4d1c0501c111b0707b6670cbdc368e43
JoshSpangler <jes3277@rit.edu>
2018-12-25 20:33:48 -0500

    Added wakeup.js file to support the !wakeup [@user] command

---
+++ commands/wakeup.js
@@ -0,0 +1,11 @@
+// Will ping (mention) a user 10 times in an attempt to "wake" the user up
+
+exports.run = (client, message, args) => {
+	var user = args[0];
+
+	for(var i = 0; i < 10; i++) {
+		message.channel.send("Wakeup " + user + " !");
+	}
+
+	return;
+}
