f9a3aabc58c0020dc4a88e6f5e68bb5987a7e6bf
joshspangler <jes3277@rit.edu>
2019-04-03 19:48:16 -0400

    Cleaner, faster

--- commands/fetchAllMessages.js
+++ commands/fetchAllMessages.js
@@ -8,24 +8,33 @@ exports.run = (client, message, args) => {
 
 	// Setup the counter, some recent message ID, and the new database
 	let counter = 0;	
-	let msgID = 562996444517367818;
+	let msgID = 563140663764582451;
 	let db = { };
 	async.whilst(
 		// We're going to grab the first 10k messages to start things off
 		function test() {
-			return counter < 10000;
+			return counter < 100;
 		},
 		// Grab the last message from the updating message ID
 		function fetch(callback) {
-			message.channel.fetchMessages({ limit: 1, before: msgID })
+			message.channel.fetchMessages({ limit: 100, before: msgID })
 				.then(function(messages) {
-					msgText = messages.first().content;
-					msgID = messages.first().id;
-					// Only store this message if it isn't blank (a picture?)
-					if(msgText) {
-						console.log(msgText);
-						db[msgID] = msgText;
-					}
+					// Update msgID with the last message in this block
+					msgID = messages.last().id;
+					// Now, we loop over each fetched message
+					let msgText, author;
+					let msgArray = messages.array();
+					msgArray.forEach(function(msg) {
+						msgText = msg.content;
+						msgID = msg.id;
+						author = msg.author;
+						// Only store this message if it isn't blank (a picture?) and
+						// we aren't looking at another bot's wacky message
+						if(msgText && !author.bot)   {
+							console.log(msgText);
+							db[msgID] = msgText;
+						}
+					});
 				})
 				.catch(console.error);
 			// Keep goin
