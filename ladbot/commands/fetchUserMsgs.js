0c686fdb4bbe61fdf2d5c5c728f50d829004a3c5
jspangled <jes3277@rit.edu>
2020-07-05 00:45:19 -0400

    Updated stuf for data collection and training

--- ladbot/commands/fetchUserMsgs.js
+++ ladbot/commands/fetchUserMsgs.js
@@ -14,7 +14,7 @@ exports.run = (client, message, args) => {
   }
 
   // get #yunglads channel
-  const yunglads = client.channels.get(client.config.youngladsID);
+  const yunglads = client.channels.cache.get(client.config.youngladsID);
 
   // some data on the user we're getting messages for
   // get non-#-non-space name.
@@ -29,69 +29,72 @@ exports.run = (client, message, args) => {
   let counter = 0;
   let done = false;
   let msgID = 719216519996899420; // set this to a recent msg ID of target
-                                  // TODO find way to find this automatically.
+                                  // TODO  find way to find this automatically.
   const userDB = [];
+  console.log("Starting message collection for this user.");
   async.whilst(
-      /**
-         grab as many messages as we can for a user.
-         it seems like most users on the lads server have no more than 80k.
-         */
-      () => counter < 1000 && done === false,
-      // while we fetch
-      (callback) => {
-        const options = {limit: 100, before: msgID};
-        // fetch from our #yunglads channel
-        yunglads.fetchMessages(options)
-            .then((messages) => {
-              const msgArray = messages.array();
-              // update msgID with the last message in this block
-              try {
-                // console.log(messages);
-                msgID = messages.last().id;
-                // now loop over each fetched message
-                for (let i = 0; i < msgArray.length; i++) {
-                  const msg = msgArray[i];
-                  /**
-                              only add a message to our userDB if it belongs to our guy
-                              and the message isnt a command
-                              */
-                  if (msg.author.id === authID && msg.content && msg.content.charAt(0) !== "!") {
-                    // TODO add more filtering
-                    if (!msg.content.includes("https://")) {
-                      // See if this message is a consecutive message from the same author.
-                      // If so, clump the message together with previous
-                      if (i > 0 && msg.author === msgArray[i - 1].author) {
-                        userDB[0].content = `${msg.content}. ${userDB[0].content}`;
-                      } else {
-                        userDB.unshift(msg);
-                      }
+    /**
+     grab as many messages as we can for a user.
+     it seems like most users on the lads server have no more than 80k.
+     */
+    function test(cb) { cb(null, counter < 10000 && done === false); },
+    // while we fetch
+    function iter(callback) {
+      const options = {limit: 100, before: msgID};
+      // fetch from our #yunglads channel
+      yunglads.messages.fetch(options)
+          .then((messages) => {
+            const msgArray = messages.array();
+            // update msgID with the last message in this block
+            try {
+              // console.log(messages);
+              msgID = messages.last().id;
+              // now loop over each fetched message
+              for (let i = 0; i < msgArray.length; i++) {
+                const msg = msgArray[i];
+                /**
+                 only add a message to our userDB if it belongs to our guy
+                 and the message isnt a command
+                 */
+                if (msg.author.id === authID && msg.content && msg.content.charAt(0) !== "!") {
+                  // TODO add more filtering
+                  if (!msg.content.includes("https://") && msg.content.split(" ").length > 3) {
+                    // See if this message is a consecutive message from the same author.
+                    // If so, clump the message together with previous
+                    if (i > 0 && msg.author === msgArray[i - 1].author) {
+                      userDB[0].content = `${msg.content}. ${userDB[0].content}`;
+                    } else {
+                      userDB.unshift(msg);
                     }
                   }
                 }
-              } catch (err) {
-                // at this point, notify that we're ready to save
-                done = true;
               }
-            })
-            .catch(console.error);
-        // keep iterating
-        counter++;
-        setTimeout(callback, 500);
-      },
-      // upon completion
-      (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        // save to disk in gpt-2 format
-        const stream = fs.createWriteStream(`../data/learn/train_data/${name}.json`, {flags: "a"});
-        userDB.forEach((msg) => {
-          stream.write(msg.content);
-          stream.write("\n<|endoftext|>\n");
-        });
-        stream.end();
-        console.log("Saved this users messages");
-      },
+            } catch (err) {
+              // at this point, notify that we're ready to save
+              console.error;
+              done = true;
+            }
+          })
+          .catch(console.error);
+      // keep iterating
+      counter++;
+      setTimeout(callback, 500);
+    },
+    // upon completion
+    function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // save to disk in gpt-2 format
+      const stream = fs.createWriteStream(`../ladbot/data/train/datasets/${name}.txt`, {flags: "a"});
+      userDB.forEach((msg) => {
+        stream.write("\n<|startoftext|>\n");
+        stream.write(msg.content);
+        stream.write("\n<|endoftext|>\n");
+      });
+      stream.end();
+      console.log("Saved this users messages.");
+    },
   );
 };
--- ladbot/learn/train.py
+++ ladbot/learn/train.py
@@ -1,4 +1,26 @@
+""" File responsible for training a GPT-2 model on a discord user's dataset. """
+
 import gpt_2_simple as gpt2
+import tensorflow as tf
+import sys
 import os
 
-function 
+def train(file_path: str):
+  model_name = "124M"
+  model_dir = "../data/train/models"
+
+  # Start training session.
+  os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+  sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
+  gpt2.finetune(sess,
+                file_path,
+                steps=5,
+                model_name=model_name,
+                model_dir=os.path.join(model_dir))
+
+def main():
+  train(str(sys.argv[1]))
+
+if __name__ == "__main__":
+  main()
