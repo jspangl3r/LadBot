478b5ceaed76e0f5cadba01339fe582f8289cd49
Josh Spangler <jes3277@rit.edu>
2021-04-14 16:13:36 -0400

    Update restart.ts

--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -2,7 +2,6 @@ import { Client, Message } from "discord.js";
 import { restart } from "../utils";
 
 export function run(client: Client, message: Message): void {
-  console.log(client);
   restart(client);
   return;
 }
